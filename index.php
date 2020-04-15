1a731a9058029094103bac98253f8c276f9f28a9
Pridestalker <6103741+Pridestalker@users.noreply.github.com>
2020-04-15 10:00:37 +0200

    Added a template for a no elementor page

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 
-$context = \Timber\Timber::get_context();
-$context['post'] = new \Timber\Post();
+use Timber\Post;
+use Timber\Timber;
 
-return \Timber\Timber::render(
-    [
-        'views/page.html.twig',
-    ],
-    $context
+$context         = Timber::get_context();
+$context['post'] = new Post();
+
+$templates = ['views/page.html.twig'];
+
+if (class_exists('\Elementor\Plugin')) {
+	if (\Elementor\Plugin::$instance->db->is_built_with_elementor($context['post']->id)) {
+		array_unshift($templates, 'views/page-no-elementor.html.twig');
+	}
+}
+
+return Timber::render(
+	apply_filters('fonk/page-controllers/index/templates', $templates),
+    apply_filters('fonk/page-controllers/index/context', $context),
+	apply_filters('fonk/page-controllers/index/cache', [false, false])
 );
